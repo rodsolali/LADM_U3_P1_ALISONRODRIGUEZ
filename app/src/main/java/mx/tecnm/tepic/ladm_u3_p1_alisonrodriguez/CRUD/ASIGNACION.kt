b16b8582f089bd14eb3d24c9242927896b914508
Alison Rodríguez <contact@itt.com>
2022-05-01 23:58:28 -0600

    ultimo

--- app/src/main/java/mx/tecnm/tepic/ladm_u3_p1_alisonrodriguez/CRUD/ASIGNACION.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u3_p1_alisonrodriguez/CRUD/ASIGNACION.kt
@@ -3,6 +3,7 @@ package mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.CRUD
 import android.content.ContentValues
 import android.content.Context
 import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.baseDatos
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.tipoDato.Asignacion
 
 class ASIGNACION(p: Context) {
     val p= p
@@ -57,5 +58,38 @@ class ASIGNACION(p: Context) {
         cursor.close()
         tabla.close()
         return lista
+    }
+    //getAll
+    fun getAll(): ArrayList<Asignacion> {
+        val tabla = baseDatos(p, "EquiposComputo", null, 1).readableDatabase
+        val columnas = arrayOf("IDASIGNACION","NOM_EMPLEADO", "AREA_TRABAJO", "FECHA", "CODIGOBARRAS")
+        val cursor = tabla.query("ASIGNACION", columnas, null, null, null, null, null)
+        val lista = ArrayList<Asignacion>()
+        if (cursor.moveToFirst()) {
+            do {
+                val asignacion = Asignacion(cursor.getInt(0),cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4))
+                lista.add(asignacion)
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        tabla.close()
+        return lista
+    }
+    //getOne by barras
+    fun getOneByBarras(barras:String): Asignacion {
+        val tabla = baseDatos(p, "EquiposComputo", null, 1).readableDatabase
+        val columnas = arrayOf("IDASIGNACION","NOM_EMPLEADO", "AREA_TRABAJO", "FECHA", "CODIGOBARRAS")
+        val cursor = tabla.query("ASIGNACION", columnas, "CODIGOBARRAS = ?", arrayOf(barras), null, null, null)
+        val asignacion = Asignacion(0,"","","","")
+        if (cursor.moveToFirst()) {
+            asignacion.id = cursor.getInt(0)
+            asignacion.empleado = cursor.getString(1)
+            asignacion.area = cursor.getString(2)
+            asignacion.fecha = cursor.getString(3)
+            asignacion.CodigoBarra = cursor.getString(4)
+        }
+        cursor.close()
+        tabla.close()
+        return asignacion
     }
 }
--- app/src/main/java/mx/tecnm/tepic/ladm_u3_p1_alisonrodriguez/listaAsignacion.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u3_p1_alisonrodriguez/listaAsignacion.kt
@@ -1,10 +1,25 @@
 package mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.annotation.Nullable
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.CRUD.ASIGNACION
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.CRUD.INVENTARIO
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.databinding.FragmentListaAsignacionBinding
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.databinding.FragmentListaInventarioBinding
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.recyclers.RecyclerAsignacion
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.recyclers.RecyclerInventario
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.tipoDato.Asignacion
+import mx.tecnm.tepic.ladm_u3_p1_alisonrodriguez.tipoDato.Inventario
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +31,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [listaAsignacion.newInstance] factory method to
  * create an instance of this fragment.
  */
-class listaAsignacion : Fragment() {
+class listaAsignacion : Fragment(), RecyclerAsignacion.onClickListener, SearchView.OnQueryTextListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,12 +44,36 @@ class listaAsignacion : Fragment() {
         }
     }
 
+    private lateinit var _binding: FragmentListaAsignacionBinding
+    private val b get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_asignacion, container, false)
+        _binding = FragmentListaAsignacionBinding.inflate(inflater, container, false)
+        return b.root
+    }
+    lateinit var listaAsignacion:MutableList<Asignacion>
+    var listaAsignacionOriginal:MutableList<Asignacion>? = null
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        setupRecyclerView()
+        b.search.setOnQueryTextListener(this)
+    }
+    private fun setupRecyclerView(){
+        b.listaAsignacion.layoutManager = LinearLayoutManager(activity?.applicationContext!!)
+        b.listaAsignacion.addItemDecoration(
+            DividerItemDecoration(activity?.applicationContext!!,
+                DividerItemDecoration.VERTICAL)
+        )
+        val i = ASIGNACION(activity?.applicationContext!!)
+        listaAsignacion = i.getAll()
+        listaAsignacionOriginal = i.getAll()
+        b.listaAsignacion.adapter = RecyclerAsignacion(activity?.applicationContext!!,listaAsignacion,this)
+
+    }
+
+    private fun RecargarRecycler(){
+        b.listaAsignacion.adapter = RecyclerAsignacion(activity?.applicationContext!!,listaAsignacion,this)
     }
 
     companion object {
@@ -55,5 +94,87 @@ class listaAsignacion : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onClick(
+        codigoBarras: String,
+        nombre: String,
+        area: String,
+        fecha: String,
+        itemView: View,
+        position: Int
+    ): Boolean {
+        val menu = PopupMenu(context,itemView)
+        val builder = AlertDialog.Builder(context)
+        val info = AlertDialog.Builder(context)
+        menu.menu.add("Editar")
+        menu.menu.add("Ver")
+        menu.menu.add("Desasignar")
+        menu.setOnMenuItemClickListener {
+            when(it.title){
+                "Desasignar" -> {
+                    builder.setTitle("Borrar")
+                    builder.setMessage("¿Esta seguro de borrar la asignacion?")
+                    builder.setPositiveButton("Si"){_,_ ->
+                        val i = ASIGNACION(activity?.applicationContext!!)
+                        i.eliminarbarras(codigoBarras)
+                        listaAsignacion.removeAt(position)
+                        RecargarRecycler()
+                    }
+                    builder.setNegativeButton("No"){_,_ ->
+                        Toast.makeText(context,"No se borro",Toast.LENGTH_SHORT).show()
+                    }
+                    builder.show()
+                }
+                "Editar" -> {
+                }
+                "Ver" -> {
+                    val i = ASIGNACION(activity?.applicationContext!!)
+                    val inventario = i.getOneByBarras(codigoBarras)
+                    info.setTitle("Informacion")
+                    info.setMessage("Codigo de barras: ${inventario.CodigoBarra}\n" +
+                            "Nombre: ${inventario.empleado}\n" +
+                            "Area: ${inventario.area}\n" +
+                            "Fecha: ${inventario.fecha}\n")
+                    info.show()
+                }
+            }
+            true
+        }
+        menu.show()
+        return true
+    }
+
+    fun filtrado(txtBuscar: String) {
+        val longitud = txtBuscar.length
+        if(longitud == 0){
+            listaAsignacion.clear()
+            listaAsignacion.addAll(listaAsignacionOriginal!!)
+        }else{
+            listaAsignacion.clear()
+            listaAsignacion.addAll(listaAsignacionOriginal!!)
+            val collecion = listaAsignacion.filter {
+                it.CodigoBarra.lowercase().contains(txtBuscar.lowercase()).or(
+                    it.area.lowercase().contains(txtBuscar.lowercase())
+                ).or(it.empleado.lowercase().contains(txtBuscar.lowercase())).or(
+                    it.fecha.lowercase().contains(txtBuscar.lowercase())
+                )
+            }
+            listaAsignacion.clear()
+            listaAsignacion.addAll(collecion)
+        }
+        RecargarRecycler()
+    }
+
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        if (p0 != null) {
+            filtrado(p0)
+        }
+        return false
     }
 }
